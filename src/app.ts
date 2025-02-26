d7ec55c3498f5edc62b6b25a300f89b1f5fbe4ed
PS <princerocks889@gmail.com>
2025-02-26 21:25:35 +0530

    On branch feature/add-eslint-pretier-husky  - configure eslint prettier and husky

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express,{Express,Request,Response} from 'express'
+import express, { Express, Request, Response } from 'express'
 
-const app:Express = express()
-
+const app: Express = express()
 
 const port = 3000
-app.get("/",(req:Request,res:Response)=>{
-    res.send("hello world2!")
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world2!')
 })
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+    console.log(`[server]: Server is running at http://localhost:${port}`)
+})
